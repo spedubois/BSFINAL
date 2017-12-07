bf2d463bc0df48a16f4c7dc7c0b2f27146d1e84a
spedubois <spenser@mechdesign.com>
2017-12-06 20:46:03 -0700

    changes

--- app/src/main/java/com/example/spenserdubois/battleship/Wait.kt
+++ app/src/main/java/com/example/spenserdubois/battleship/Wait.kt
@@ -3,6 +3,7 @@ package com.example.spenserdubois.battleship
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
@@ -12,20 +13,19 @@ class Wait : AppCompatActivity() {
 
     private lateinit var fireAuth : FirebaseAuth
     private lateinit var fireDB : DatabaseReference
+    var canceled = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fireAuth = FirebaseAuth.getInstance()
         fireDB = FirebaseDatabase.getInstance().reference
         val user = fireAuth.currentUser
-
-        val btnCancel = btnCancel
-
         if(user !is FirebaseUser)
             return
 
         setContentView(R.layout.activity_wait)
 
         var gameId = textGameID
+        val cancel = btnCancel as Button
         var s = intent.getStringExtra("GameID")
         gameId.text="GameID : "+s
 
@@ -40,7 +40,7 @@ class Wait : AppCompatActivity() {
 
                 var p2Name = data.value.toString()
 
-                if(!p2Name.equals("")) {
+                if(!p2Name.equals("") && !canceled) {
                     var intent = Intent(this@Wait, MainActivity::class.java)
                     intent.putExtra("GameID", s)
                     startActivity(intent)
@@ -51,9 +51,10 @@ class Wait : AppCompatActivity() {
 
         })
 
-        btnCancel.setOnClickListener {
-            fireDB.child("Games").child(s).setValue("")
-            var intent = Intent(this@Wait, BeginActivity::class.java)
+        cancel.setOnClickListener {
+            canceled = true
+            fireDB.child("Games").child(s).setValue(null)
+            var intent = Intent(this@Wait, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
